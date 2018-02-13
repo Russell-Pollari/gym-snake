56e80725656f5f65eb2bf1a811636d4762919dc1
russell-pollari <pollarir@mgail.com>
2018-02-12 22:59:02 -0800

    Add randomly placed fruit which grow snake

--- gym_snake/envs/snake_env.py
+++ gym_snake/envs/snake_env.py
@@ -9,18 +9,30 @@ class SnakeEnv(gym.Env):
 
     def __init__(self, board_shape=(10, 10)):
         self.board_shape = board_shape
+        self.board = np.zeros(board_shape)
         self.info = {}
         self._reset()
 
     def _reset(self):
         self.snake = [[0, 2], [0, 1], [0, 0]]
-        self._update_snake_position()
         self.reward = 0
         self.done = False
+        self._place_fruit()
+        self._update_snake_position()
         return self.board
+
+    def _place_fruit(self):
+        valid_position = False
+        while not valid_position:
+            x = np.random.randint(0, self.board_shape[0] - 1)
+            y = np.random.randint(0, self.board_shape[1] - 1)
+            valid_position = self.board[x, y] != 1
+        self.fruit_pos = [x, y]
 
     def _update_snake_position(self):
         self.board = np.zeros(self.board_shape)
+        (fruit_x, fruit_y) = self.fruit_pos
+        self.board[fruit_x, fruit_y] = 2
         for x, y in self.snake:
             self.board[x, y] = 1
 
@@ -44,12 +56,15 @@ class SnakeEnv(gym.Env):
         if self.board[x, y] == 1:
             self.done = True
 
-        if not self.done:
+        # if snake eats fruit
+        if self.board[x, y] == 2:
             self.reward += 1
+            self._place_fruit()
+        else:
+            self.snake.pop()
 
         new_head = [x, y];
         self.snake.insert(0, new_head)
-        self.snake.pop()
         self._update_snake_position()
 
         return self.board, self.reward, self.done, self.info
